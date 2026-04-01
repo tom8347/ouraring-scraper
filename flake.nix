649013201a76bfc32b4f506e71f22dbb9d280c79
Tom Knapton <tomk9277+github@gmail.com>
2026-04-01 19:06:17 +0100

    change to darwin

--- flake.nix
+++ flake.nix
@@ -5,28 +5,36 @@
 
   outputs = { self, nixpkgs }:
   let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-    py = pkgs.python3.withPackages (ps: with ps; [
-      requests
-      numpy
-      pandas
-      matplotlib
-      pynvim
-      python-lsp-server
-      pyqt6
-    ]);
+    systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
   in
   {
-    devShells.${system}.default = pkgs.mkShell {
-      packages = [ py pkgs.qt6.qtwayland ];
+    devShells = forAllSystems (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+      isLinux = pkgs.stdenv.isLinux;
+      py = pkgs.python3.withPackages (ps: with ps; [
+        requests
+        numpy
+        pandas
+        matplotlib
+        pyqt6
+      ]);
+      linuxPkgs = pkgs.lib.optionals isLinux [ pkgs.qt6.qtwayland ];
+    in
+    {
+      default = pkgs.mkShell {
+        packages = [ py ] ++ linuxPkgs;
 
-      shellHook = ''
-        echo "Oura scraper environment"
-        echo "Python: $(python --version)"
-        export QT_QPA_PLATFORM=wayland
-        export MPLBACKEND=QtAgg
-      '';
-    };
+        shellHook = ''
+          echo "Oura scraper environment"
+          echo "Python: $(python --version)"
+          ${pkgs.lib.optionalString isLinux ''
+            export QT_QPA_PLATFORM=wayland
+          ''}
+          export MPLBACKEND=QtAgg
+        '';
+      };
+    });
   };
 }
